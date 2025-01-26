45e959007c21b170faf86863c0266a3db110023f
Steven <46513711+stvnchng@users.noreply.github.com>
2025-01-25 19:36:08 -0600

    tree is done

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const info = document.getElementById("info");
 const WIDTH = fractal.clientWidth * 0.6;
 const MIDPOINT = fractal.clientWidth / 2;
 
+const SLIDER_MAX = 12;
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return `${this.x},${this.y}`;
+  }
+}
+
 function drawLine(start, end, y) {
   const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
   line.setAttribute("x1", start);
@@ -21,28 +34,23 @@ function drawCantorSet(start, end, y, depth = 0) {
   if (depth >= depthSlider.value) {
     // base case: C_0 = [start, end]
     drawLine(start, end, y);
-  } else {
-    const third = (end - start) / 3;
-    // cantor set: C_n+1 = (1/3)C_n U (2/3) + (1/3)C_n
-    drawCantorSet(start, start + third, y + 20, depth + 1);
-    drawCantorSet(start + 2 * third, end, y + 20, depth + 1);
-    drawLine(start, end, y);
+    return;
   }
+  const third = (end - start) / 3;
+  // cantor set: C_n+1 = (1/3)C_n U (2/3) + (1/3)C_n
+  drawCantorSet(start, start + third, y + 20, depth + 1);
+  drawCantorSet(start + 2 * third, end, y + 20, depth + 1);
+  drawLine(start, end, y);
 }
 
 function drawTriangle(x, y, size, color) {
-  const height = (Math.sqrt(3) / 2) * size;
-  const points = `${x},${y} ${x + size / 2},${y + height} ${x - size / 2},${
-    y + height
-  }`;
-
-  console.log(points);
-
+  const h = (Math.sqrt(3) / 2) * size; // equilateral triangle
+  const pts = `${x},${y} ${x + size / 2},${y + h} ${x - size / 2},${y + h}`;
   const triangle = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "polygon"
   );
-  triangle.setAttribute("points", points);
+  triangle.setAttribute("points", pts);
   triangle.setAttribute("fill", color);
   fractal.appendChild(triangle);
 }
@@ -51,29 +59,18 @@ function drawSierpinski(x, y, size, depth) {
   const height = (Math.sqrt(3) / 2) * size;
   if (depth <= 0) {
     drawTriangle(x, y, size, "black");
-  } else {
-    const halfSize = size / 2;
-    const halfHeight = height / 2;
-
-    drawSierpinski(x, y, halfSize, depth - 1); // Top
-    drawSierpinski(x - halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-left
-    drawSierpinski(x + halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-right
+    return;
   }
+  const halfSize = size / 2;
+  const halfHeight = height / 2;
+
+  drawSierpinski(x, y, halfSize, depth - 1); // Top
+  drawSierpinski(x - halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-left
+  drawSierpinski(x + halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-right
 }
 
 function drawSnowflake(x, y, size, depth) {
   // TODO
-}
-
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  toString() {
-    return `${this.x},${this.y}`;
-  }
 }
 
 function drawSquare(points) {
@@ -111,39 +108,21 @@ function drawPythagoras(size, rotation, ctrX, ctrY, depth = 0) {
 
   if (depth >= depthSlider.value) {
     drawSquare(rotatedPoints);
-  } else {
-    const newSize = size * Math.sqrt(0.5);
-
-    // Calculate offsets for child branches
-    const angle = (rotation * Math.PI) / 180;
-    const leftCtrX = ctrX - halfSize * Math.cos(angle);
-    const leftCtrY = ctrY - halfSize * Math.sin(angle);
-
-    const rightCtrX = ctrX + halfSize * Math.cos(angle);
-    const rightCtrY = ctrY + halfSize * Math.sin(angle);
-
-    drawPythagoras(newSize, rotation + 45, leftCtrX, leftCtrY, depth + 1);
-    drawPythagoras(newSize, rotation - 45, rightCtrX, rightCtrY, depth + 1);
-
-    drawSquare(rotatedPoints);
+    return;
   }
+  const newSize = size / Math.sqrt(2);
+
+  const lX = ctrX + newSize * Math.cos(((rotation + 45) * Math.PI) / 180);
+  const lY = ctrY + newSize * Math.sin(((rotation + 45) * Math.PI) / 180);
+
+  const rX = ctrX + newSize * Math.cos(((rotation - 45) * Math.PI) / 180);
+  const rY = ctrY + newSize * Math.sin(((rotation - 45) * Math.PI) / 180);
+
+  drawSquare(rotatedPoints);
+
+  drawPythagoras(newSize, rotation + 45, lX, lY, depth + 1);
+  drawPythagoras(newSize, rotation - 45, rX, rY, depth + 1);
 }
-
-fractalSelect.addEventListener("change", () => {
-  fractal.innerHTML = "";
-  depthSlider.value = 0;
-  depthSlider.dispatchEvent(new Event("input"));
-  draw();
-});
-
-depthSlider.addEventListener("input", () => {
-  fractal.innerHTML = "";
-  const startTime = performance.now();
-  draw();
-  info.textContent = `Depth: ${depthSlider.value} | Time to render: ${(
-    performance.now() - startTime
-  ).toFixed(2)}ms`;
-});
 
 function draw() {
   switch (fractalSelect.value) {
@@ -157,7 +136,43 @@ function draw() {
       drawSnowflake(MIDPOINT, 0, WIDTH, Number(depthSlider.value));
       break;
     case "tree":
-      drawPythagoras(100, 0, fractal.clientWidth / 2, fractal.clientHeight / 2);
+      drawPythagoras(
+        150,
+        -90,
+        fractal.clientWidth / 2,
+        fractal.clientHeight / 2
+      );
+      break;
+    case "carpet":
+      // TODO
       break;
   }
 }
+
+fractalSelect.addEventListener("change", () => {
+  fractal.innerHTML = "";
+  depthSlider.value = 0;
+  depthSlider.disabled = false;
+  depthSlider.dispatchEvent(new Event("input"));
+  draw();
+});
+
+depthSlider.addEventListener("input", () => {
+  fractal.innerHTML = "";
+  const startTime = performance.now();
+  draw();
+  info.textContent = `Depth: ${depthSlider.value} | Time to render: ${(
+    performance.now() - startTime
+  ).toFixed(2)}ms`;
+});
+
+// initialize slider
+depthSlider.max = SLIDER_MAX;
+const ticks = document.getElementById("ticks");
+Array.from({ length: SLIDER_MAX + 1 }).forEach((_, i) => {
+  ticks.appendChild(
+    Object.assign(document.createElement("option"), {
+      value: i,
+    })
+  );
+});
