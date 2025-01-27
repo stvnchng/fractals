c50a1a22c18f11376febad21266011b3ede271c9
Steven <46513711+stvnchng@users.noreply.github.com>
2025-01-26 18:34:39 -0600

    using p5 instead because canvas > svg

--- main.js
+++ main.js
@@ -1,146 +1,73 @@
 const fractalSelect = document.getElementById("fractal-select");
-const fractal = document.getElementById("fractal");
 const depthSlider = document.getElementById("depthSlider");
 const info = document.getElementById("info");
 
-const WIDTH = fractal.clientWidth * 0.6;
-const MIDPOINT = fractal.clientWidth / 2;
+const WIDTH = window.innerWidth * 0.8;
+const HEIGHT = window.innerHeight * 0.7;
+const MIDPOINT = WIDTH / 2;
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+const p = new p5((p) => {
+  p.setup = setup;
+  p.draw = drawFractal;
+});
 
-  toString() {
-    return `${this.x},${this.y}`;
-  }
+function setup() {
+  p.createCanvas(WIDTH, HEIGHT);
+  p.background(255);
+  p.noLoop();
 }
 
 function drawLine(start, end, y) {
-  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  line.setAttribute("x1", start);
-  line.setAttribute("x2", end);
-  line.setAttribute("y1", y);
-  line.setAttribute("y2", y);
-  line.setAttribute("stroke", "black");
-  line.setAttribute("stroke-width", "4");
-  fractal.appendChild(line);
+  p.stroke(randomRGB());
+  p.strokeWeight(4);
+  p.line(start, y, end, y);
 }
 
 function drawCantorSet(start, end, y, depth = 0) {
   if (depth >= depthSlider.value) {
-    // base case: C_0 = [start, end]
     drawLine(start, end, y);
     return;
   }
   const third = (end - start) / 3;
-  // cantor set: C_n+1 = (1/3)C_n U (2/3) + (1/3)C_n
   drawCantorSet(start, start + third, y + 20, depth + 1);
   drawCantorSet(start + 2 * third, end, y + 20, depth + 1);
   drawLine(start, end, y);
 }
 
 function drawTriangle(x, y, size, color) {
-  const h = (Math.sqrt(3) / 2) * size; // equilateral triangle
-  const pts = `${x},${y} ${x + size / 2},${y + h} ${x - size / 2},${y + h}`;
-  const triangle = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "polygon"
-  );
-  triangle.setAttribute("points", pts);
-  triangle.setAttribute("fill", color);
-  fractal.appendChild(triangle);
+  const height = (Math.sqrt(3) / 2) * size;
+  p.fill(color);
+  p.triangle(x, y, x - size / 2, y + height, x + size / 2, y + height);
 }
 
 function drawSierpinski(x, y, size, depth) {
   const height = (Math.sqrt(3) / 2) * size;
-  if (depth <= 0) {
-    drawTriangle(x, y, size, "black");
+  if (depth === 0) {
+    drawTriangle(x, y, size, "blue");
     return;
   }
-  const halfSize = size / 2;
+  const halfBaseLen = size / 2;
   const halfHeight = height / 2;
 
-  drawSierpinski(x, y, halfSize, depth - 1); // Top
-  drawSierpinski(x - halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-left
-  drawSierpinski(x + halfSize / 2, y + halfHeight, halfSize, depth - 1); // Bottom-right
+  drawSierpinski(x, y, halfBaseLen, depth - 1); // Top
+  drawSierpinski(x - halfBaseLen / 2, y + halfHeight, halfBaseLen, depth - 1); // Bottom-left
+  drawSierpinski(x + halfBaseLen / 2, y + halfHeight, halfBaseLen, depth - 1); // Bottom-right
 }
 
-function drawSnowflake(x, y, size, depth) {
-  // TODO
-}
-
-function drawSquare(points, fill = "none", stroke = "blue") {
-  const pts = points.map((pt) => pt.toString()).join(" ");
-  const square = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "polygon"
-  );
-  square.setAttribute("points", pts);
-  square.setAttribute("fill", fill);
-  square.setAttribute("stroke", stroke);
-  fractal.appendChild(square);
-}
-
-function rotatePoint(x, y, ctrX, ctrY, angle) {
-  const rad = (angle * Math.PI) / 180;
-  const newX = ctrX + (x - ctrX) * Math.cos(rad) - (y - ctrY) * Math.sin(rad);
-  const newY = ctrY + (x - ctrX) * Math.sin(rad) + (y - ctrY) * Math.cos(rad);
-  return new Point(newX, newY);
-}
-
-function drawTree(size, rotation, ctrX, ctrY, depth = 0) {
-  const halfSize = size / 2;
-
-  const points = [
-    new Point(ctrX - halfSize, ctrY + halfSize), // Bottom-left
-    new Point(ctrX + halfSize, ctrY + halfSize), // Bottom-right
-    new Point(ctrX + halfSize, ctrY - halfSize), // Top-right
-    new Point(ctrX - halfSize, ctrY - halfSize), // Top-left
-  ];
-
-  const rotatedPoints = points.map((pt) =>
-    rotatePoint(pt.x, pt.y, ctrX, ctrY, rotation)
-  );
-
-  if (depth >= depthSlider.value) {
-    drawSquare(rotatedPoints);
-    return;
-  }
-  const newSize = size / Math.sqrt(2);
-
-  const lX = ctrX + newSize * Math.cos(((rotation + 45) * Math.PI) / 180);
-  const lY = ctrY + newSize * Math.sin(((rotation + 45) * Math.PI) / 180);
-
-  const rX = ctrX + newSize * Math.cos(((rotation - 45) * Math.PI) / 180);
-  const rY = ctrY + newSize * Math.sin(((rotation - 45) * Math.PI) / 180);
-
-  drawSquare(rotatedPoints);
-
-  drawTree(newSize, rotation + 45, lX, lY, depth + 1);
-  drawTree(newSize, rotation - 45, rX, rY, depth + 1);
-}
-
-function drawRect(x, y, width, height, fill = "purple") {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", x); // top-left corner
-  rect.setAttribute("y", y);
-  rect.setAttribute("width", width);
-  rect.setAttribute("height", height);
-  rect.setAttribute("fill", fill);
-  fractal.appendChild(rect);
+function drawRect(x, y, width, height, fill = "purple", stroke = "purple") {
+  p.fill(fill);
+  p.stroke(stroke);
+  p.rect(x, y, width, height);
 }
 
 function drawCarpet(x, y, size, depth = 0) {
   // base case: draw big square with one hole in the middle
   // recursive: plop that hole on all the surrounding mini squares
-  if (depth <= 0) {
+  if (depth === 0) {
     drawRect(x, y, size, size);
     return;
   }
   const third = size / 3;
-  drawRect(x, y, size, size);
   drawCarpet(x, y, third, depth - 1); // top left
   drawCarpet(x + third, y, third, depth - 1); // top mid
   drawCarpet(x + 2 * third, y, third, depth - 1); // top right
@@ -152,31 +79,102 @@ function drawCarpet(x, y, size, depth = 0) {
   drawRect(x + third, y + third, third, third, "white");
 }
 
-function draw() {
+function drawSquare(x, y, size, rotation, fill = "brown", stroke = "green") {
+  p.push(); // apply transforms only to currently drawn shape
+  p.translate(x, y);
+  p.stroke(stroke);
+  p.fill(fill);
+  p.rotate(toRadians(rotation));
+  p.square(-size / 2, -size / 2, size);
+  p.pop();
+}
+
+function drawTree(cx, cy, size, rotation, depth = 0) {
+  drawSquare(cx, cy, size, rotation);
+
+  if (depth >= Number(depthSlider.value)) return;
+
+  const newSize = size / Math.sqrt(2);
+
+  const lX = cx + newSize * Math.cos(toRadians(rotation + 45));
+  const lY = cy + newSize * Math.sin(toRadians(rotation + 45));
+  const rX = cx + newSize * Math.cos(toRadians(rotation - 45));
+  const rY = cy + newSize * Math.sin(toRadians(rotation - 45));
+
+  drawTree(lX, lY, newSize, rotation + 45, depth + 1);
+  drawTree(rX, rY, newSize, rotation - 45, depth + 1);
+}
+
+function drawSnowflake(x, y, size, depth) {
+  // TODO
+}
+
+// function drawCircle(cx, cy, r, fill = "none", stroke = "blue") {
+//   p.fill(fill);
+//   p.stroke(stroke);
+//   p.ellipse(cx, cy, r * 2, r * 2);
+// }
+
+// function drawGasket(x, y, radius, depth = 0) {
+//   if (depth <= 0) {
+//     drawCircle(x, y, radius);
+//     return;
+//   }
+//   drawCircle(x, y, radius);
+//   // r = (R*sqrt3) / (sqrt3 + 2)
+//   const childRadius = (radius * Math.sqrt(3)) / (Math.sqrt(3) + 2);
+//   const dist = radius - childRadius;
+//   drawGasket(
+//     x - dist * Math.cos(toRadians(30)),
+//     y - dist * Math.sin(toRadians(30)),
+//     childRadius,
+//     depth - 1
+//   );
+//   drawGasket(
+//     x + dist * Math.cos(toRadians(30)),
+//     y - dist * Math.sin(toRadians(30)),
+//     childRadius,
+//     depth - 1
+//   );
+//   drawGasket(x, y + dist, childRadius, depth - 1);
+// }
+
+function drawFractal() {
+  resetCanvas();
+
   const depth = Number(depthSlider.value);
+  const DEFAULT_SIZE = Math.min(WIDTH, HEIGHT);
   switch (fractalSelect.value) {
     case "sierpinski":
-      drawSierpinski(MIDPOINT, 0, WIDTH, depth);
+      drawSierpinski(MIDPOINT, 0, DEFAULT_SIZE, depth);
       break;
     case "cantor":
-      drawCantorSet(0, fractal.clientWidth, 50);
+      drawCantorSet(0, WIDTH, 50);
       break;
     case "tree":
-      drawTree(150, -90, fractal.clientWidth / 2, fractal.clientHeight / 2);
+      drawTree(WIDTH / 2, HEIGHT / 2, 150, -90);
       break;
     case "carpet":
-      const CARPET_SIZE = 600;
       drawCarpet(
-        fractal.clientWidth / 2 - CARPET_SIZE / 2,
-        fractal.clientHeight / 2 - CARPET_SIZE / 2,
-        CARPET_SIZE,
+        WIDTH / 2 - DEFAULT_SIZE / 2,
+        HEIGHT / 2 - DEFAULT_SIZE / 2,
+        DEFAULT_SIZE,
         depth
       );
       break;
     case "snowflake":
       drawSnowflake(MIDPOINT, 0, WIDTH, depth);
       break;
+    // case "gasket":
+    //   const RADIUS = DEFAULT_SIZE / 2;
+    //   drawGasket(WIDTH / 2, HEIGHT / 2, RADIUS, depth);
   }
+}
+
+function resetCanvas() {
+  p.clear();
+  p.noStroke();
+  p.noFill();
 }
 
 // required since some fractals go too hard
@@ -205,21 +203,32 @@ function setLimits() {
 }
 
 fractalSelect.addEventListener("change", () => {
-  fractal.innerHTML = "";
   depthSlider.value = 0;
   depthSlider.disabled = false;
   depthSlider.dispatchEvent(new Event("input"));
   setLimits();
-  draw();
+  drawFractal();
 });
 
 depthSlider.addEventListener("input", () => {
-  fractal.innerHTML = "";
   const startTime = performance.now();
-  draw();
+  drawFractal();
   info.textContent = `Depth: ${depthSlider.value} | Time to render: ${(
     performance.now() - startTime
   ).toFixed(2)}ms`;
 });
 
 setLimits();
+
+// misc. helpers
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function toRadians(degs) {
+  return (degs * Math.PI) / 180;
+}
